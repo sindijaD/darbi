db7757d2d85facbd4e5226d5d0a941fcf441bdbb
austrisD <austris.daugulis@gmail.com>
2020-06-10 12:50:32 +0300

    emojy fix

--- Test_JS/index.js
+++ Test_JS/index.js
@@ -16,10 +16,27 @@ const   input = document.querySelector('#range'),
 input.addEventListener('input', 
     function(e){
         rating2(e.target.value);
-    }  
+    } 
 );
 function rating2(rating){
-    const score = document.querySelector('.score');
+    const score = document.querySelector('.score'),
+    emoji1 = document.querySelector('.emoji1'),
+    emoji2 = document.querySelector('.emoji2'),
+    emoji3 = document.querySelector('.emoji3'),
+    emoji4 = document.querySelector('.emoji4'),
+    emoji5 = document.querySelector('.emoji5'),
+    emoji6 = document.querySelector('.emoji6'),
+    emoji7 = document.querySelector('.emoji7'),
+    emoji8 = document.querySelector('.emoji8'),
+    emoji9 = document.querySelector('.emoji9'),
+    emoji10 = document.querySelector('.emoji10');
+    function returnDefault(rez, loc){
+        if(rating == rez){
+            loc.style.opacity = "100%";
+        }else{
+            loc.style.opacity = "20%";
+        }
+    };
     switch(rating){
         case'1':score.innerHTML = '<p>'+rating + score1;break;
         case'2':score.innerHTML = '<p>'+rating + score2;break;
@@ -33,7 +50,31 @@ function rating2(rating){
         case'10':score.innerHTML = '<p>'+rating + score10;break;
         default:'klūda';
     };
+    returnDefault(1, emoji1);
+    returnDefault(2, emoji2);
+    returnDefault(3, emoji3);
+    returnDefault(4, emoji4);
+    returnDefault(5, emoji5);
+    returnDefault(6, emoji6);
+    returnDefault(7, emoji7);
+    returnDefault(8, emoji8);
+    returnDefault(9, emoji9);
+    returnDefault(10, emoji10);
 };
+/*
+
+    function returnDefault(rez,loc){
+        if(rating == rez){
+            loc.outerHTML = '<div style="opacity: 100%;" class="'+loc+'"></div>';
+        }else{
+            loc.outerHTML = '<div class="'+loc+'"></div>';
+        };
+    };
+    returnDefault(1,emoji1);
+emoji1 = document.querySelector('.emoji1');
+emoji1.outerHTML = '<div class="emoji1"></div>';
+emoji1.outerHTML = '<div style="opacity: 100%;" class="emoji1"></div>';
+*/
 /*
 function rating2(rating){
     const score = document.querySelector('.score');
