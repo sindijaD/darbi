56d2f4d29acbc6a200cccb2073681069dc45a4f0
austrisD <austris.daugulis@gmail.com>
2020-09-04 18:55:52 +0300

    Updates

---
+++ portfolio_sites/landing/readMe.txt
@@ -0,0 +1,8 @@
+Only works with jekyll command in terminal
+--  bundle exec jekyll serve      (run with gems enabled)
+--  jekyll build                      (build website)
+
+
+
+--  bundle update                         (update gems)
+--  bundle install
---
+++ precache-manifest.bd8941c58ad08cc2bdadbc5d969d211a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9de2866cb7c0c105207aa3ca5cfd79",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cd823fb7f6d870b77bce",
+    "url": "/portfolio/static/css/main.06906e10.chunk.css"
+  },
+  {
+    "revision": "e56b8bd59c13c73bd300",
+    "url": "/portfolio/static/js/2.baeddf00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.baeddf00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd823fb7f6d870b77bce",
+    "url": "/portfolio/static/js/main.92d42664.chunk.js"
+  },
+  {
+    "revision": "fc7703655368873a1e87",
+    "url": "/portfolio/static/js/runtime-main.d24d1c12.js"
+  },
+  {
+    "revision": "0a1ee68111416520f9a7020fb409c67f",
+    "url": "/portfolio/static/media/Avatar.0a1ee681.JPG"
+  },
+  {
+    "revision": "712e5c61b3511b5ad6b78da1359e00ca",
+    "url": "/portfolio/static/media/Orbitron-Black.712e5c61.ttf"
+  },
+  {
+    "revision": "add0c4244a015960586eeb51f091fd71",
+    "url": "/portfolio/static/media/Orbitron-Regular.add0c424.ttf"
+  },
+  {
+    "revision": "173deda76bdb9ae0337aa8d71cbf832e",
+    "url": "/portfolio/static/media/landing_prew.173deda7.jpg"
+  },
+  {
+    "revision": "fd7ce6693d4497ca8d8ba5d0243fd859",
+    "url": "/portfolio/static/media/login_prew.fd7ce669.jpg"
+  },
+  {
+    "revision": "03fb4d4940e0e3eb4f9299196f5001c2",
+    "url": "/portfolio/static/media/profile_prew.03fb4d49.jpg"
+  },
+  {
+    "revision": "b0cbad9e1a89335154b3004d795e0773",
+    "url": "/portfolio/static/media/todolistPrew.b0cbad9e.jpg"
+  },
+  {
+    "revision": "87e52866eff3d6fcfc57428a4db7ebcf",
+    "url": "/portfolio/static/media/webskola.lv_prew.87e52866.jpg"
+  }
+]);
